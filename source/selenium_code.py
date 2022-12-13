5b1566446282ca44bbc6ca2f82ec113aac1fe826
Florian Kretschmer <florian.kretschmer@posteo.de>
2022-12-13 17:16:45 +0100

    remove unnecessary imports

--- source/selenium_code.py
+++ source/selenium_code.py
@@ -2,12 +2,10 @@ import time
 import logging
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
 
