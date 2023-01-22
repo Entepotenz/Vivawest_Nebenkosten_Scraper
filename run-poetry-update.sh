87d58ef24acb259608448e1a894922b041b37744
Florian Kretschmer <florian.kretschmer@posteo.de>
2023-01-22 14:45:47 +0100

    update and upgrades and get intellij environment running

--- run-poetry-update.sh
+++ run-poetry-update.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-docker run --rm -it -v "$(pwd)/:/source" python:3-slim bash -c "\
+docker run --rm -it -v "$(pwd)/:/source" -v "/source/.venv" python:3-slim bash -c "\
     apt-get update; \
     pip install poetry; \
     cd /source; \
