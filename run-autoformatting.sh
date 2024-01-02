f71df4600757e98394dbe666361021061aa82ea8
F.K. <19738301+Entepotenz@users.noreply.github.com>
2024-01-02 10:35:08 +0100

    apply shfmt

--- run-autoformatting.sh
+++ run-autoformatting.sh
@@ -5,21 +5,21 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if ! command -v yapf &> /dev/null; then
-    docker run --rm -it -v "$(pwd)/source:/source" python:3-slim bash -c "\
+if ! command -v yapf &>/dev/null; then
+  docker run --rm -it -v "$(pwd)/source:/source" python:3-slim bash -c "\
         pip install yapf; \
         yapf --style google -ir /source"
 else
-    yapf --style google -ir "$SCRIPT_DIR/source"
+  yapf --style google -ir "$SCRIPT_DIR/source"
 fi
 
-if ! command -v black &> /dev/null; then
-    docker run --rm -it -v "$(pwd)/source:/source" python:3-slim bash -c "\
+if ! command -v black &>/dev/null; then
+  docker run --rm -it -v "$(pwd)/source:/source" python:3-slim bash -c "\
         apt-get update && apt-get install -y --no-install-recommends build-essential; \
         pip install black; \
         black /source"
 else
-    black "$SCRIPT_DIR/"
+  black "$SCRIPT_DIR/"
 fi
--- run-docker-container.sh
+++ run-docker-container.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 readonly DOCKER_IMAGE_NAME="vivawest_scraper"
 readonly PATH_TO_DOCKERFILE="$SCRIPT_DIR/Dockerfile"
 readonly PATH_TO_PASS_SH="$SCRIPT_DIR/pass.sh"
@@ -15,22 +15,22 @@ touch "$PATH_TO_PASS_SH"
 chmod u=rw,g=,o= "$PATH_TO_PASS_SH"
 
 if [ "$#" -eq 1 ]; then
-    if [[ -n "$1" ]]; then
-        if [ "$1" = "rebuild" ]; then
-          if docker images | tr -s ' ' : | grep -q "$DOCKER_IMAGE_NAME"; then
-            images_to_delete=$(docker images | tr -s ' ' : | grep $DOCKER_IMAGE_NAME | cut -f3 -d:)
-            for image_id in $images_to_delete; do
-              docker image rm "$image_id"
-            done
-          fi
-          docker build -t "$DOCKER_IMAGE_NAME" --file "$PATH_TO_DOCKERFILE" .
-        else
-            echo "\"$1\" -> unsupported argument"
-        fi
+  if [[ -n "$1" ]]; then
+    if [ "$1" = "rebuild" ]; then
+      if docker images | tr -s ' ' : | grep -q "$DOCKER_IMAGE_NAME"; then
+        images_to_delete=$(docker images | tr -s ' ' : | grep $DOCKER_IMAGE_NAME | cut -f3 -d:)
+        for image_id in $images_to_delete; do
+          docker image rm "$image_id"
+        done
+      fi
+      docker build -t "$DOCKER_IMAGE_NAME" --file "$PATH_TO_DOCKERFILE" .
+    else
+      echo "\"$1\" -> unsupported argument"
     fi
+  fi
 fi
 
-if [[ "$(docker images -q "$DOCKER_IMAGE_NAME" 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q "$DOCKER_IMAGE_NAME" 2>/dev/null)" == "" ]]; then
   docker build -t "$DOCKER_IMAGE_NAME" --file "$PATH_TO_DOCKERFILE" .
 fi
 
