b5c95bc3758fab6a87b45725476ac3bc01ea5186
F.K. <19738301+Entepotenz@users.noreply.github.com>
2024-11-16 19:13:01 +0100

    enable headless mode

--- source/scraping.py
+++ source/scraping.py
@@ -10,7 +10,7 @@ import selenium_code
 def scrape_site(url_to_scrape: str, username: str, password: str) -> dict:
     result = {}
 
-    driver = selenium_code.run_selenium_init(url_to_scrape, headless=False)
+    driver = selenium_code.run_selenium_init(url_to_scrape, headless=True)
     selenium_code.run_selenium_login(driver, username, password)
     bearer_token = selenium_code.get_authorization_bearer(driver)
     result["heizenergie"] = get_heizenergie(driver, bearer_token)
