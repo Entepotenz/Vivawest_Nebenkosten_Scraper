e10e37b19adefe21a07d0c1a1296fe040f347a25
F.K. <19738301+Entepotenz@users.noreply.github.com>
2024-11-16 19:17:30 +0100

    remove unused code

--- source/app.py
+++ source/app.py
@@ -1,28 +1,19 @@
 import os
 
 import scraping
-from flask import Flask
 
 URL = "https://kundenportal.vivawest.de/"
-
-app = Flask(__name__)
 
 # launch with this command inside the `source` folder
 # poetry run gunicorn --bind 0.0.0.0:8080 --timeout 90 --reload app:app
 
 
-@app.route("/")
 def get_as_html():
     return get_as_json()
 
 
-@app.route("/json")
 def get_as_json():
     username = os.environ.get("USERNAME", None)
     password = os.environ.get("PASSWORD", None)
 
     return scraping.scrape_site(URL, username, password)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
