4cc3219747b4051b168c62b31d207e9b32d99ea1
Florian Kretschmer <19738301+Entepotenz@users.noreply.github.com>
2025-06-05 18:11:54 +0200

    chore(deps): update python docker tag to v3.13.3 (#194)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- run-autoformatting.sh
+++ run-autoformatting.sh
@@ -7,7 +7,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-DOCKER_IMAGE="python:3.13.0-slim@sha256:4efa69bf17cfbd83a9942e60e2642335c3b397448e00410063a0421f9727c4c4"
+DOCKER_IMAGE="python:3.13.3-slim@sha256:56a11364ffe0fee3bd60af6d6d5209eba8a99c2c16dc4c7c5861dc06261503cc"
 
 if ! command -v yapf &>/dev/null; then
   docker run --rm -it -v "$(pwd)/source:/source" $DOCKER_IMAGE bash -c "\
