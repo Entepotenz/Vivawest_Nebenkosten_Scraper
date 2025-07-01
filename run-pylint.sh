b2842537336c8d64ed32b749e3b3141c7c800110
Florian Kretschmer <19738301+Entepotenz@users.noreply.github.com>
2025-07-01 19:55:10 +0200

    chore(deps): update python docker tag to v3.13.5 (#271)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- run-pylint.sh
+++ run-pylint.sh
@@ -7,7 +7,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-DOCKER_IMAGE="python:3.13.3-slim@sha256:56a11364ffe0fee3bd60af6d6d5209eba8a99c2c16dc4c7c5861dc06261503cc"
+DOCKER_IMAGE="python:3.13.5-slim@sha256:f2fdaec50160418e0c2867ba3e254755edd067171725886d5d303fd7057bbf81"
 
 if ! command -v pylint &>/dev/null; then
   docker run --rm -it -v "$(pwd)/source:/source" \
