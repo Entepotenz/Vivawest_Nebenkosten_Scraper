8447aafe6a166066150472c8a7b904b2035e3392
Florian Kretschmer <19738301+Entepotenz@users.noreply.github.com>
2026-01-04 17:11:47 +0100

    chore(deps): update python:3.14.2-slim docker digest to 3955a7d (#363)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- run-pylint.sh
+++ run-pylint.sh
@@ -7,7 +7,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-DOCKER_IMAGE="python:3.14.2-slim@sha256:aa5be1196770ff8c5896e3da0848332cd73663a99a69fc7a2b6772e53111793c"
+DOCKER_IMAGE="python:3.14.2-slim@sha256:3955a7dd66ccf92b68d0232f7f86d892eaf75255511dc7e98961bdc990dc6c9b"
 
 if ! command -v pylint &>/dev/null; then
   docker run --rm -it -v "$(pwd)/source:/source" \
